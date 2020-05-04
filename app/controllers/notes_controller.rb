b5551df7206f84cef70f7012c3815b69236e8656
mpottebaum <mpottebaum@gmail.com>
2020-05-04 10:28:47 -0500

    setup notes and users controllers

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,17 @@
+class NotesController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+end
