56b41fb0e7170695e21bb6dc1bba16bbb97d02e2
mpottebaum <mpottebaum@gmail.com>
2020-05-05 13:03:48 -0500

    added tags to note model and controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,13 +2,13 @@ class NotesController < ApplicationController
 
     def index
         user = User.find(params[:user_id])
-        render json: user.notes
+        render json: user.notes, include: [:tags]
     end
 
     def create
         user = User.find(params[:user_id])
         note = user.notes.create(notes_params)
-        render json: note
+        render json: note, include: [:tags]
     end
 
     def show
@@ -18,19 +18,19 @@ class NotesController < ApplicationController
         note = Note.find(params[:id])
         note.update(notes_params)
         user = User.find(params[:user_id])
-        render json: user.notes
+        render json: user.notes, include: [:tags]
     end
 
     def destroy
         note = Note.find(params[:id])
         note.destroy
         user = User.find(params[:user_id])
-        render json: user.notes
+        render json: user.notes, include: [:tags]
     end
 
     private
 
     def notes_params
-        params.require(:note).permit(:title, :content)
+        params.require(:note).permit(:title, :content, :tags_attributes)
     end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,10 @@ class Note < ApplicationRecord
 
     has_many :note_tags
     has_many :tags, through: :note_tags
+
+    def tags_attributes=(tags_attributes)
+        tags_attributes.each do |tag_attributes|
+            tags << Tag.find_or_create_by(name: tag_attributes.name)
+        end
+    end
 end
