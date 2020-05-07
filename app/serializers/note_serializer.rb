9b0e62aa4de574133b3735128d3a9d06a610a29e
mpottebaum <mpottebaum@gmail.com>
2020-05-07 17:33:13 -0500

    added timestamps to note hash

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,5 @@
 class NoteSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :tags
+  attributes :id, :title, :content, , :created_at, :updated_at, :tags
 
   def tags
     self.object.tags
