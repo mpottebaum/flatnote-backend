1d59975bbdf797b4a5a5cfac1a45770416b41be2
mpottebaum <mpottebaum@gmail.com>
2020-05-07 10:44:00 -0500

    converted database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_04_151304) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "note_tags", force: :cascade do |t|
     t.integer "note_id"
     t.integer "tag_id"
